abe4e7293665c17110af0aa33527902b2f43caa0
Lucas Barros <lucsbarros@gmail.com>
2015-03-22 15:59:49 -0300

    create mainnav toggle function and animation

--- scripts/home.js
+++ scripts/home.js
@@ -4,30 +4,42 @@
       return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     }
 
-    // get element
     var getElement = function(elm) {
-      return document.querySelectorAll(elm);
+      return document.querySelector(elm);
     }
     
     var getElementHeight = function(_elm) {
-      elm = getElement(_elm)[0];
+      elm = getElement(_elm);
       return elm.innerHeight || elm.clientHeight;
     }
     
     var getElementWidth = function(_elm) {
-      elm = getElement(_elm)[0];
+      elm = getElement(_elm);
       return elm.innerWidth || elm.clientWidth;
     }
+    
+    var toggleClass = function(elm, clss) {
+      return elm.classList.toggle(clss);
+    }
+    
+    //sets
 
     var setPageOffset = function() {
-      getElement('.home-header')[0].nextElementSibling.style.marginTop = getPageHeight() + "px";
+      getElement('.home-header').nextElementSibling.style.marginTop = getPageHeight() + "px";
     }
     
     var setElementOffset = function(elm) {
-      getElement(elm)[0].style.marginTop = getElementHeight(elm) * -0.5 + "px";
+      getElement(elm).style.marginTop = getElementHeight(elm) * -0.5 + "px";
+    }
+    
+    var setToggleMenu = function() {
+      return getElement('.page-nav .btn-toggle').onclick = function() {
+        toggleClass(getElement('.page-nav'), 'active');
+      }
     }
 
     //executes
+    setToggleMenu();
     setPageOffset();
     setElementOffset('.header-description');
   
